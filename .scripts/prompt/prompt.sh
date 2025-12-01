a410095c2a2055fc278ff1ac2fa9e13d8ec6d5d9
Holler János <hollerjanos97@gmail.com>
2025-12-01 11:38:51 +0100

    Unnecessary part has been removed
    There was a part from the previous version, it
    has been removed.

--- .scripts/prompt/prompt.sh
+++ .scripts/prompt/prompt.sh
@@ -7,14 +7,6 @@ if [[ ! -z "${1}" ]]; then
     question=${1}
 fi
 
-
-while getopts 'q:h' flag; do
-    case "${flag}" in
-        q) question="${OPTARG}" ;;
-        h|*) printUsage; exit 1 ;;
-    esac
-done
-
 read -p "${question} [Y|n] " -n 1 -r -s confirm
 echo
 confirm=${confirm:-Y}
